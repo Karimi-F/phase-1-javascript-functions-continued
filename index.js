20f67cebcc0f1e165f88e48efe4f1c2cc5182c3f
Karimi-F <fridah37karimi@gmail.com>
2024-09-25 12:50:13 +0300

    Completes functions continued

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun (activity = `roller-skate`){
+    
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+function mondayWork (work = `go to the office`){
+    return `This Monday, I will ${work}.`
+}
+mondayWork()
+
+function wrapAdjective(asterisk = "*") {
+    // Inner function (wrap) is returned
+    return function(adjective = " special") {
+        return `You are ${asterisk}${adjective}${asterisk}!`;
+    };
+}
+wrapAdjective ()("a hard worker")
+wrapAdjective("||")("a dedicated programmer")
+console.log(wrapAdjective()("a hard worker"));
+console.log(wrapAdjective("||")("a dedicated programmer"));
